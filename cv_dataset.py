7d1cba11329fa6d34a55929818a95a7dbf59a7dd
chaejun Seo <ghskfen06@naver.com>
2023-03-18 22:25:00 +0900

    Add files via upload

---
+++ cv_dataset.py
@@ -0,0 +1,41 @@
+from torch.utils.data import Dataset, DataLoader
+import cv2
+import os
+import torch
+import torchvision
+from torchvision import transforms # 이미지 데이터 augmentation
+import os
+import glob
+
+
+class Custom_dataset(Dataset):
+  def __init__(self, root_path, mode, transforms):
+    self.all_data = sorted( glob.glob( os.path.join(root_path, mode, '*', '*') ) ) # 전체 경로
+    self.transforms = transforms
+
+  def __getitem__(self, index):
+    if torch.is_tensor(index): # 인덱스가 tensor형태일 수 있는 것을 방지
+      index = index.tolist()
+
+    data_path = self.all_data[index] # 인덱스가 부여된 데이터 1개
+    image = cv2.imread(data_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # cv2 라이브러리의 BGR 형식을 RGB로 permute
+
+    # transfrom 적용
+    if self.transforms is not None:
+      augmentation = self.transforms(image = image)
+      image = augmentation['image']
+
+    # 이미지의 이름을 이용하여, label 부여하기
+    if 'dolphin' in data_path:
+      label = 0
+    elif 'shark' in data_path:
+      label = 1
+    else:
+      label = 2
+
+    return image, label
+
+  def __len__(self):
+    length = len(self.all_data)
+    return length
